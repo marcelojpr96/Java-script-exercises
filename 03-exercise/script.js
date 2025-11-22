b6f1dad1b58bbfa496512bde8b3cd3bc4bf520d3
marcelojpr96 <marcelojpr96@gmail.com>
2025-11-22 08:14:25 +0000

    we got buttons working with document.queryselector aswell as a function to reverse the name, it differs from python in the sense that we cant use str[::-1] to reverse it, instead we have to split it and reverse it and join it at the end, likeso : str.spli('').reverse().join('')

--- 03-exercise/script.js
+++ 03-exercise/script.js
@@ -4,3 +4,18 @@ const this_wont_change = test;
 console.log(this_wont_change);
 test = "oops it changed"
 console.log(this_wont_change, test);
+
+const button = document.querySelector("button");
+function greet(){
+    const name = prompt("what is your name?");
+    const greeting = document.querySelector("#hellomate");
+    greeting.textContent = `hello ${name}, greetings`;
+    
+}
+button.addEventListener("click", greet);
+let firstname = "roger";
+function reversename(firstname){
+    return firstname.split('').reverse().join('');
+}
+let reversed_name = reversename(firstname);
+console.log("This is you name", firstname, reversed_name);
