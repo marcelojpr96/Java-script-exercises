bc5509bcf27ba5e1f33f104b7954181fe00affdf
marcelojpr96 <marcelojpr96@gmail.com>
2025-11-22 07:27:56 +0000

    some playing around with js

--- 02-exercise/script.js
+++ 02-exercise/script.js
@@ -1,4 +1,14 @@
 let firstname = "marcelo";
 let lastname ="rachado";
 
+const thiswontchange = 500;
+let math = 0.5;
+
 console.log(firstname, lastname);
+console.log(thiswontchange*math);
+console.log("assigning something else to math, and then trying to assign something else to const");
+math=5;
+console.log(thiswontchange*math);
+math ="something else";
+console.log(`can i typechange a variable? ${math}`)
+//thiswontchange=5; //this will error out the code
