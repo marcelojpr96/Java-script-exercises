3eb745e395ac804092c9640dc323c383428492f4
marcelojpr96 <marcelojpr96@gmail.com>
2025-12-16 23:17:30 +0000

    preparation for a new problem

---
+++ 10-image-Gallery/script.js
@@ -0,0 +1,15 @@
+
+    // pic1.jpg: "Closeup of a human eye".
+    // pic2.jpg: "Rock that looks like a wave".
+    // pic3.jpg: "Purple and white pansies".
+    // pic4.jpg: "Section of wall from a pharaoh's tomb".
+    // pic5.jpg: "Large moth on a leaf".
+
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+console.log("hi")
