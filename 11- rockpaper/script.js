f880c7f446913d44e655a99297b67d11b01d155b
marcelojpr96 <marcelojpr96@gmail.com>
2025-12-18 15:29:34 +0000

    missing javascript stuff now

---
+++ 11- rockpaper/script.js
@@ -0,0 +1,17 @@
+const playerOneScore = document.querySelector(".player-one-score");
+const playerTwoScore = document.querySelector(".player-two-score");
+const playerOneChoiceImg = document.querySelector(".player-one-choice");
+const playerTwoChoiceImg = document.querySelector(".player-two-choice");
+//start the images hidden
+playerOneChoiceImg.style.visibility = "hidden";
+playerTwoChoiceImg.style.visibility = "hidden";
+
+const gun_item = document.querySelector(".box.eigth");
+/*
+gun_item.style.visibility = "hidden";
+*/
+// gun_item.addEventListener("click",()=>{
+    
+//     playerOneChoiceImg.src="./imgs/item_rock.png";
+// });
+console.log(playerOneScore.textContent);
